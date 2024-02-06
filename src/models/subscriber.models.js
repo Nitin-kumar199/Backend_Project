e18f642bb2935328d485332f0262260971728251
Nitin Kumar <199nitinkumar@gmail.com>
2024-02-06 11:18:12 +0530

    get channel controller done

--- src/models/subscriber.models.js
+++ src/models/subscriber.models.js
@@ -3,10 +3,12 @@ import mongoose, { Schema } from "mongoose";
 const subscriberSchema = new Schema(
   {
     subscriber: {
+      // one who is subscribing
       type: Schema.Types.ObjectId,
       ref: "User",
     },
     channel: {
+      // one whom to subscriber is subscribing
       type: Schema.Types.ObjectId,
       ref: "User",
     },
