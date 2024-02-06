6cf38a39f49fb46536ad744260d10b02a872541c
Nitin Kumar <199nitinkumar@gmail.com>
2024-02-06 12:32:04 +0530

    router done

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   logoutUser,
   registerUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { varifyJWT } from "../middleware/auth.middleware.js";
@@ -27,5 +34,16 @@ router.route("/login").post(loginUser);
 //secure route
 router.route("/logout").post(varifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(varifyJWT, changeCurrentPassword);
+router.route("/current-user").get(varifyJWT, getCurrentUser);
+router.route("/update-account").patch(varifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(varifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(varifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/:username").get(varifyJWT, getUserChannelProfile);
+router.route("/history").get(varifyJWT, getWatchHistory);
 //http://localhost:/api/v1/users/register
 export default router;
